bdb7a5bf45bd66c8d7c95530fda9a588734dfeda
Daniel Posadas Rangel <95835551+DanielPosR@users.noreply.github.com>
2023-05-19 23:55:01 -0600

    agregando videos para que se muestren al abrir el sitio por primera vez

--- src/App.js
+++ src/App.js
@@ -53,15 +53,63 @@ function App() {
     },
     {
       id: uuid(),
+      categoria: 'Front End',
+      link: 'https://www.youtube.com/watch?v=AG2QssLpQzI',
+      imagen: 'https://i.ytimg.com/vi/AG2QssLpQzI/maxresdefault.jpg'
+    },
+    {
+      id: uuid(),
+      categoria: 'Front End',
+      link: 'https://www.youtube.com/watch?v=JJgcJ9g-a30',
+      imagen: 'https://i.ytimg.com/vi/JJgcJ9g-a30/maxresdefault.jpg'
+    },
+    {
+      id: uuid(),
+      categoria: 'Front End',
+      link: 'https://www.youtube.com/watch?v=j71g5TiMA-M',
+      imagen: 'https://i.ytimg.com/vi/j71g5TiMA-M/maxresdefault.jpg'
+    },
+    {
+      id: uuid(),
       categoria: 'Back End',
       link: 'https://www.youtube.com/watch?v=Z024LSCMqFk',
       imagen: 'https://i.ytimg.com/vi/Z024LSCMqFk/maxresdefault.jpg'
     },
     {
       id: uuid(),
+      categoria: 'Back End',
+      link: 'https://www.youtube.com/watch?v=eejIHbOk_uI',
+      imagen: 'https://i.ytimg.com/vi/eejIHbOk_uI/maxresdefault.jpg'
+    },
+    {
+      id: uuid(),
+      categoria: 'Back End',
+      link: 'https://www.youtube.com/watch?v=3aK3Omo2RrU',
+      imagen: 'https://i.ytimg.com/vi/3aK3Omo2RrU/maxresdefault.jpg'
+    },
+    {
+      id: uuid(),
+      categoria: 'Innovación y Gestión',
+      link: 'https://www.youtube.com/watch?v=pOYyav4qx7o',
+      imagen: 'https://i.ytimg.com/vi/pOYyav4qx7o/maxresdefault.jpg'
+    },
+    {
+      id: uuid(),
+      categoria: 'Innovación y Gestión',
+      link: 'https://www.youtube.com/watch?v=mwNfWRsDDWE',
+      imagen: 'https://i.ytimg.com/vi/mwNfWRsDDWE/maxresdefault.jpg'
+    },
+    {
+      id: uuid(),
       categoria: 'Innovación y Gestión',
       link: 'https://www.youtube.com/watch?v=vhwspfvI52k',
       imagen: 'https://i.ytimg.com/vi/vhwspfvI52k/maxresdefault.jpg'
+    },
+    {
+      id: uuid(),
+      categoria: 'Innovación y Gestión',
+      link: 'https://www.youtube.com/watch?v=7lnmW8fB0nM',
+      imagen: 'https://i.ytimg.com/vi/7lnmW8fB0nM/maxresdefault.jpg'
     }
   ]);
 
