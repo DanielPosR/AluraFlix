c4cd5664381d25eeb19e85c0bb555d9aecfd8d51
Daniel Posadas Rangel <95835551+DanielPosR@users.noreply.github.com>
2023-05-19 20:19:48 -0600

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,152 @@
 import './App.css';
 import './Normalize.css';
-import Header from './Components/Header';
-import Hero from './Components/Hero';
+import React, { useEffect } from 'react';
+import Header from './Components/Header/Header';
+import PieDePagina from './Components/Footer/Footer';
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+import NuevoVideo from './Formularios/FormVideo/NuevoVideo';
+import NuevaCategoria from './Formularios/FormCategoria/NuevaCategoria';
+import Home from './Pages/Home';
+import Error from './Pages/404';
+
+
 
 function App() {
+
+  const getCategorias = JSON.parse(localStorage.getItem('Categorias'));
+  const getVideos = JSON.parse(localStorage.getItem('Videos'));
+
+  const [categorias, setCategorias] = useState(getCategorias || [
+    {
+      id: uuid(),
+      titulo: "Front End",
+      colorPrimario: "#6BD1FF",
+      descripcion: "Formación Front End de Alura Latam"
+    },
+    {
+      id: uuid(),
+      titulo: "Back End",
+      colorPrimario: "#00C86F",
+      descripcion: "Formación Back End de Alura Latam"
+    },
+    {
+      id: uuid(),
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#FF8C2A",
+      descripcion: "Formación Innovación y Gestión de Alura Latam"
+    }
+  ]);
+
+
+
+
+  const [videos, setVideos] = useState(getVideos || [
+    {
+      id: uuid(),
+      categoria: 'Front End',
+      link: 'https://www.youtube.com/watch?v=-LmFK6skG7s',
+      imagen: 'https://i.ytimg.com/vi/-LmFK6skG7s/maxresdefault.jpg'
+    },
+    {
+      id: uuid(),
+      categoria: 'Back End',
+      link: 'https://www.youtube.com/watch?v=Z024LSCMqFk',
+      imagen: 'https://i.ytimg.com/vi/Z024LSCMqFk/maxresdefault.jpg'
+    },
+    {
+      id: uuid(),
+      categoria: 'Innovación y Gestión',
+      link: 'https://www.youtube.com/watch?v=vhwspfvI52k',
+      imagen: 'https://i.ytimg.com/vi/vhwspfvI52k/maxresdefault.jpg'
+    }
+  ]);
+
+
+
+  //*useEffect para localStorage
+  useEffect(() => {
+    localStorage.setItem('Videos', JSON.stringify(videos));
+  }, [videos]);
+
+  useEffect(() => {
+    localStorage.setItem('Categorias', JSON.stringify(categorias));
+  }, [categorias]);
+
+
+  const guardarVideo = (nuevoVideo) => {
+
+    setVideos([...videos, { ...nuevoVideo, id: uuid() }]);
+  }
+
+  function guardarCategoria(nuevaCategoria) {
+
+    setCategorias([...categorias, { ...nuevaCategoria, id: uuid() }]);
+  }
+
+  function eliminarCategoria(id) {
+
+    const nuevoCategorias = categorias.filter((categoria) => categoria.id !== id)
+
+    setCategorias(nuevoCategorias)
+  }
+
+
+  function eliminarVideo(id) {
+
+    const confirmar = window.confirm('¿Deseas eliminar este video?');
+
+    if (confirmar) {
+      const videoEliminado = videos.filter((video) => video.id !== id);
+      return setVideos(videoEliminado);
+    } else {
+      return;
+    }
+  }
+
   return (
-    <div className="App">
+
+    <Router>
+
       <Header />
-      <Hero />
-    </div>
+
+      <Routes>
+
+        <Route
+          path='/'
+          element={<Home
+            categorias={categorias}
+            videos={videos}
+            eliminarVideo={eliminarVideo}
+          />}
+        />
+
+        <Route
+          path='/video'
+          element={<NuevoVideo
+            guardarVideo={guardarVideo}
+            categorias={categorias} />}
+        />
+
+        <Route
+          path='/categoria'
+          element={<NuevaCategoria
+            guardarCategoria={guardarCategoria}
+            categorias={categorias}
+            eliminarCategoria={eliminarCategoria} />}
+        />
+
+        <Route path='*' element={<Error />} />
+
+      </Routes>
+
+      <PieDePagina />
+
+    </Router>
+
   );
 }
 
