e81087fc073c7bf38beb539c04eb9a4a98961bb6
Daniel Posadas Rangel <95835551+DanielPosR@users.noreply.github.com>
2023-06-05 20:56:34 -0600

    Agregando nombre al Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,18 +5,27 @@ const Footer = styled.footer`
     padding-bottom: 6rem;
     background-color: #000;
     display: flex;
-    justify-content: center;
+    align-items: center;
+    flex-direction: column;
     border-top: 3px solid #2A7AE4;
     padding: 1.5rem 0 3.5rem 0;
     margin-top: 5rem;
 `;
 
+const Parrafo = styled.p `
+    color: white;
+    padding-top: 2rem;
+    font-size: 1.8rem;
+    font-weight: bold;
+`
 
 const PieDePagina = () => {
 
     return (
         <Footer>
             <Logotipo footer={true}/>
+
+            <Parrafo>Desarrollado por Daniel Posadas Rangel | 2023 </Parrafo>
         </Footer>
     )
 }
